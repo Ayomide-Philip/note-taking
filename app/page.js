ce76d992c043c6e1e9934df14418dfcaea2b06b7
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-29 19:07:05 +0100

    feat: integrate ModalOverlay component into NoteApp structure

--- app/page.js
+++ app/page.js
@@ -1,40 +1,10 @@
 import { FaRegBookmark, FaEdit, FaTrash } from "react-icons/fa";
+import ModalOverLay from "./componet/modalOverlay";
 
 export default function NoteApp() {
   return (
-    <div className="min-h-screen flex flex-col lg:flex-row bg-gray-900 text-white">
-      <header className="lg:hidden fixed top-0 left-0 right-0 z-50 bg-gray-800 shadow-md p-4 flex justify-between items-center">
-        <h1 className="text-xl font-semibold">NotesApp</h1>
-        <nav className="flex gap-4 text-sm">
-          <a href="#" className="hover:text-blue-400">
-            All Notes
-          </a>
-          <a href="#" className="hover:text-blue-400">
-            Favorites
-          </a>
-          <a href="#" className="hover:text-blue-400">
-            Trash
-          </a>
-        </nav>
-      </header>
-
-      <aside className="hidden lg:flex fixed top-0 left-0 h-full w-64 bg-gray-800 shadow-md flex-col z-40">
-        <div className="p-6 font-bold text-2xl border-b border-gray-700">
-          NotesApp
-        </div>
-        <nav className="p-4 space-y-4">
-          <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">
-            All Notes
-          </a>
-          <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">
-            Favorites
-          </a>
-          <a href="#" className="block px-4 py-2 rounded hover:bg-gray-700">
-            Trash
-          </a>
-        </nav>
-      </aside>
-
+    <>
+      {/* <ModalOverLay /> */}
       <main className="flex-1  flex flex-col lg:ml-64 pt-16 lg:pt-0">
         <div className="p-4  bg-gray-800 shadow-md hidden lg:flex justify-between items-center">
           <h2 className="text-xl font-semibold">All Notes</h2>
@@ -103,10 +73,6 @@ export default function NoteApp() {
           ))}
         </section>
       </main>
-
-      <button className="fixed bottom-6 right-6 bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg text-xl">
-        +
-      </button>
-    </div>
+    </>
   );
 }
