1e5f5540fc4ab52aa92eff2395a28ba441bb2228
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-29 20:31:05 +0100

    feat: add nextId prop to NewPostModal for enhanced functionality

--- app/componet/newPostOverlay.jsx
+++ app/componet/newPostOverlay.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function NewPostModal({ isNewPost, setNewNote }) {
+export default function NewPostModal({ isNewPost, setNewNote, nextId }) {
   const [heading, setHeading] = useState("");
   const [noteBody, setNoteBody] = useState("");
   return (
@@ -22,11 +22,17 @@ export default function NewPostModal({ isNewPost, setNewNote }) {
           type="text"
           placeholder="Note Title"
           className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(e) => {
+            setHeading(e.target.value);
+          }}
         />
 
         <textarea
           placeholder="What's on your mind?"
           className="w-full h-32 px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
+          onChange={(e) => {
+            setNoteBody(e.target.value);
+          }}
         />
 
         <div className="mt-6 flex justify-end gap-4">
