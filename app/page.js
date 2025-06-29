8a31e0dffc0a9b25ef4722c354368bf002668039
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-29 20:05:54 +0100

    feat: add a floating action button for creating new notes in NoteApp component

--- app/page.js
+++ app/page.js
@@ -105,6 +105,10 @@ export default function NoteApp() {
           ))}
         </section>
       </main>
+      <button className="fixed bottom-6 right-6 bg-blue-600 hover:bg-blue-700 text-white p-4 w-16 h-16 rounded-full shadow-lg text-xl">
+        +
+      </button>
+      
     </>
   );
 }
