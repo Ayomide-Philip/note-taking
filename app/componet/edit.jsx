e70841b340e5a37e0072ce392ebd7044c8d435f2
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-01 00:12:37 +0100

    style: change button label from 'Post' to 'Edit' in Edit component

--- app/componet/edit.jsx
+++ app/componet/edit.jsx
@@ -66,7 +66,7 @@ export default function Edit({
               toast("Editted Sucessfully 🎉🎉🎉");
             }}
           >
-            Post
+            Edit
           </button>
         </div>
       </div>
